24d086a7bf27000a705967815aeed4d82c01c264
Akshay Raju <akshayvg4195@gmail.com>
2023-05-07 21:32:56 -0400

    Updates

---
+++ _next/static/1LID6lhj_IOabAFWnJpFA/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fabout","\u002Fabout\u002F[id]","\u002Fposts\u002F[id]","\u002Fprivacy-policy","\u002Fresources","\u002Fservices"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
